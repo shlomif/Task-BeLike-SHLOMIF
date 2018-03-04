d620744baf490bb8f4b0afbe0362af9567d9248c
Shlomi Fish <shlomif@shlomifish.org>
2018-03-04 20:28:29 +0200

    Add and fix.

--- Task-BeLike-SHLOMIF/lib/Task/BeLike/SHLOMIF.pm
+++ Task-BeLike-SHLOMIF/lib/Task/BeLike/SHLOMIF.pm
@@ -5,3 +5,24 @@ use warnings;
 
 1;
 
+=head1 NAME
+
+Task::BeLike::SHLOMIF - Be like SHLOMIF / Shlomi Fish.
+
+=pkgroup Shlomi Fish's Favourite Dists
+
+=pkg Dist::Zilla::PluginBundle::SHLOMIF
+
+=pkg Pod::Xhtml
+
+=pkg SQ
+
+=pkg Task::FreecellSolver::Testing
+
+=pkg Task::Sites::ShlomiFish
+
+=pkg Term::ReadLine::Gnu
+
+=pkg Term::ReadLine::Perl5
+
+=cut
